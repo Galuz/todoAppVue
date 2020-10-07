3956b24e0aea6cab5558be998eb960c1c84f2344
German Gonzalez <german@vetta.io>
2020-10-06 23:37:54 -0500

    main - fist commit todo app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <input class="new-todo"
+      v-model="newTodo"
+      @keyup.enter="addTodo()">
+    </header>
+    <section>
+      <ul class="todo-list">
+        <li 
+          v-for="todo in todos"
+          :key="todo.id"
+          class="todo">
+          <div>
+            <input class="toggle" type="checkbox" v-model="todo.completed">
+            <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+            <button 
+              class="destroy"
+              @click="removeTodo(todo)">X</button>
+          </div>
+          <input class="update" type="text"
+            v-model="todo.title"
+            @blur="doneEdit(todo)"
+            @keyup.enter="doneEdit(todo)">
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const STORAGE_KEY = 'todo-storage'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      newTodo: '',
+      todos:[],
+      editedTodo: ''
+    }
+  },
+  mounted(){
+    console.log("simon")
+    this.todos = JSON.parse(localStorage.getItem(STORAGE_KEY)||'[]')
+  },
+  methods: {
+    addTodo(){
+      this.todos.push({title: this.newTodo, completed: 'false', id: this.todos.length})
+      this.newTodo = ''
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos))
+    },
+    removeTodo(todo){
+      this.todos.splice(this.todos.indexOf(todo), 1);
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos))
+    },
+    editTodo(todo){
+      this.editedTodo = todo;
+    },
+    doneEdit(todo){
+      if(!this.editedTodo){
+        return
+      }
+      this.editedTodo = null
+      todo.title = todo.title.trim()
+      if(!todo.title){
+        this.removeTodo(todo)
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
